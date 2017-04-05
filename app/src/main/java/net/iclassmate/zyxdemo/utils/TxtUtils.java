bcd120edbef85e805771bc6477b162c4605b1a30
zhangyuxuan1024 <zhangyuxuan1024@163.com>
2017-04-05 15:59:16 +0800

    新建的TxtUtils类，上传至GitHub

---
+++ app/src/main/java/net/iclassmate/zyxdemo/utils/TxtUtils.java
@@ -0,0 +1,11 @@
+package net.iclassmate.zyxdemo.utils;
+
+/**
+ * Created by xydbj on 2017.4.5.
+ */
+
+public class TxtUtils {
+    private int age;
+    private String name;
+    private String id;
+}
